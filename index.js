086c9bddce69700d2f7312c1b48b09dc3ee8df52
alaydili <alaydili@usc.edu>
2020-05-15 23:43:15 -0700

    Configurations added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { User } = require('./model/user');
 
 mongoose.connect(
-    'mongodb+srv://node-shop:' + 
-    process.env.MONGO_ATLAS_PW + 
-    '@node-rest-shop-2eja9.mongodb.net/test?retryWrites=true&w=majority', 
+    config.mongoURI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true 
@@ -14,10 +16,31 @@ mongoose.connect(
 .then(() => console.log('DB connected'))
 .catch(err => console.error('Error connecting DB'));
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 });
 
 
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if(err) return res.json ({
+            success: false,
+            err
+        });
+        return res.status(200).json({
+            success: True,
+            userData: doc
+        })
+    })
+
+})
 
 app.listen(5000);
