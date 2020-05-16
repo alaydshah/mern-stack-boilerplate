cf6dfcd8018d3d24a9f7ab89f7fee3ec8245e959
alaydili <alaydili@usc.edu>
2020-05-16 03:58:25 -0700

    heroku again

--- server/index.js
+++ server/index.js
@@ -97,6 +97,10 @@ app.get('/api/user/logout', auth, (req, res) => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.json({"Message": "Deploy Successful"})
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server Running at ${port}`);
